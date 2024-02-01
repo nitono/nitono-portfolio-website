15b55179742d0aa969718c3478f5cdf32465a2f0
nitono <pyto015@ya.ru>
2024-02-01 19:25:37 +0300

    chore(edit next.config.mjs): disable build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	compress: true,
-}
+  compress: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
